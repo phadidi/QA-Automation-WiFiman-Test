63891c2c52291e642520915ca15985c9f9405ab6
phadidi <hadidip@hotmail.com>
2020-08-20 14:30:13 -0700

    Completed Optional Bonus Challenge

--- test_optional.py
+++ test_optional.py
@@ -2,6 +2,7 @@ import unittest
 
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -19,9 +20,11 @@ driver = webdriver.Remote(
 class test_optional(unittest.TestCase):
     def test_launch(self):
         assert driver
-    def test_search(self): #TODO: perform Google search and assert plot under description element
+
+    def test_search(self):
         driver.get("https://google.com")
-        driver.find_element_by_id("TODO: put search id here").send_keys("Tenet Movie Warner Brothers")
-        # get plot element
-        plot = "dummy"
+        searchBar = driver.find_element_by_name('q')
+        searchBar.send_keys("Tenet Movie Warner Brothers")
+        searchBar.send_keys(Keys.RETURN)
+        plot = str(driver.find_element_by_xpath("//span[contains(@class, 'kno-rdesc')]").text)
         assert plot == "Tenet is an upcoming spy film written, produced, and directed by Christopher Nolan. It is a co-production between the United Kingdom and United States, and stars John David Washington, Robert Pattinson, Elizabeth Debicki, Dimple Kapadia, Michael Caine, and Kenneth Branagh."
